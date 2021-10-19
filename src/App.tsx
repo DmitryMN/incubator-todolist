3327d6112cafca30d88de204362afa74141e6bc2
MalenkovDN <m18dm.dm@gmail.com>
2021-10-19 21:49:53 +0300

    add changeFilter

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,42 @@
 import React, { useState } from 'react';
-import ToDoList from "./components/ToDoList";
+import ToDoList, {TasksType} from "./components/ToDoList";
 import './App.css';
+
+export type FilterValuesType = "all" | "active" | "completed";
 
 function App() {
 
-    let tasks1 = [
+    let [tasks, setTasks] = useState<Array<TasksType>>([
         {id: 1, title: "CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "React", isDone: true},
         {id: 4, title: "Redux", isDone: false}
-    ];
+    ]);
+    const [filter, setFilter] = useState<FilterValuesType>("all");
 
+    let tasksForTodolist = tasks;
 
-    let [tasks, setTasks] = useState(tasks1);
+    switch(filter) {
+        case "active":
+            tasksForTodolist = tasks.filter(task => !task.isDone);
+            break;
+        case "completed":
+            tasksForTodolist = tasks.filter(task => task.isDone);
+            break;
+    }
+
 
     function removeTask(id: number) {
-        console.log(tasks);
         setTasks(tasks.filter(task => task.id !== id));
+    }
+
+    function changeFilter(value: FilterValuesType) {
+        setFilter(value);
     }
 
     return (
         <div className="App">
-            <ToDoList title="What to learn" tasks={tasks} remove_task={removeTask}/>
+            <ToDoList title="What to learn" tasks={tasksForTodolist} remove_task={removeTask} changeFilter={changeFilter}/>
         </div>
     );
 }
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import {FilterValuesType} from "../App";
 
-type TasksType = {
+export type TasksType = {
     id: number,
     title: string,
     isDone: boolean
@@ -9,7 +10,8 @@ type TasksType = {
 type propsToDoListType = {
     title: string,
     tasks: Array<TasksType>,
-    remove_task: (id: number) => void
+    remove_task: (id: number) => void,
+    changeFilter: (value: FilterValuesType) => void
 }
 
 function ToDoList(props: propsToDoListType) {
@@ -27,9 +29,9 @@ function ToDoList(props: propsToDoListType) {
                 </li>)}
             </ul>
             <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <button onClick={() => {props.changeFilter("all")}}>All</button>
+                <button onClick={() => {props.changeFilter("active")}}>Active</button>
+                <button onClick={() => {props.changeFilter("completed")}}>Completed</button>
             </div>
         </div>
     );
